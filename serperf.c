6230916916951da10d598d72546684e792404884
Hernán Gonzalez <hernan@vanguardiasur.com.ar>
2017-12-11 12:19:54 -0300

    serperf: Add crc8()
    Signed-off-by: Hernán Gonzalez <hernan@vanguardiasur.com.ar>

--- serperf.c
+++ serperf.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -40,13 +41,13 @@ struct arguments {
 
 struct msg_header {
 	int type;
-	int len;
-	int crc;
+	size_t len;
+	unsigned int crc;
 };
 
 struct msg {
 	struct msg_header header;
-	char payload[1024];
+	unsigned char payload[1024];
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -76,9 +77,48 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
 static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
+static unsigned char const crc8_table[] = {
+	0xea, 0xd4, 0x96, 0xa8, 0x12, 0x2c, 0x6e, 0x50, 0x7f, 0x41, 0x03, 0x3d,
+	0x87, 0xb9, 0xfb, 0xc5, 0xa5, 0x9b, 0xd9, 0xe7, 0x5d, 0x63, 0x21, 0x1f,
+	0x30, 0x0e, 0x4c, 0x72, 0xc8, 0xf6, 0xb4, 0x8a, 0x74, 0x4a, 0x08, 0x36,
+        0x8c, 0xb2, 0xf0, 0xce, 0xe1, 0xdf, 0x9d, 0xa3, 0x19, 0x27, 0x65, 0x5b,
+	0x3b, 0x05, 0x47, 0x79, 0xc3, 0xfd, 0xbf, 0x81, 0xae, 0x90, 0xd2, 0xec,
+        0x56, 0x68, 0x2a, 0x14, 0xb3, 0x8d, 0xcf, 0xf1,	0x4b, 0x75, 0x37, 0x09,
+	0x26, 0x18, 0x5a, 0x64, 0xde, 0xe0, 0xa2, 0x9c, 0xfc, 0xc2, 0x80, 0xbe,
+        0x04, 0x3a, 0x78, 0x46, 0x69, 0x57, 0x15, 0x2b, 0x91, 0xaf, 0xed, 0xd3,
+	0x2d, 0x13, 0x51, 0x6f, 0xd5, 0xeb, 0xa9, 0x97, 0xb8, 0x86, 0xc4, 0xfa,
+	0x40, 0x7e, 0x3c, 0x02, 0x62, 0x5c, 0x1e, 0x20, 0x9a, 0xa4, 0xe6, 0xd8,
+	0xf7, 0xc9, 0x8b, 0xb5, 0x0f, 0x31, 0x73, 0x4d, 0x58, 0x66, 0x24, 0x1a,
+	0xa0, 0x9e, 0xdc, 0xe2, 0xcd, 0xf3, 0xb1, 0x8f,	0x35, 0x0b, 0x49, 0x77,
+	0x17, 0x29, 0x6b, 0x55, 0xef, 0xd1, 0x93, 0xad, 0x82, 0xbc, 0xfe, 0xc0,
+	0x7a, 0x44, 0x06, 0x38, 0xc6, 0xf8, 0xba, 0x84, 0x3e, 0x00, 0x42, 0x7c,
+	0x53, 0x6d, 0x2f, 0x11, 0xab, 0x95, 0xd7, 0xe9,	0x89, 0xb7, 0xf5, 0xcb,
+	0x71, 0x4f, 0x0d, 0x33,	0x1c, 0x22, 0x60, 0x5e, 0xe4, 0xda, 0x98, 0xa6,
+	0x01, 0x3f, 0x7d, 0x43, 0xf9, 0xc7, 0x85, 0xbb, 0x94, 0xaa, 0xe8, 0xd6,
+	0x6c, 0x52, 0x10, 0x2e, 0x4e, 0x70, 0x32, 0x0c,	0xb6, 0x88, 0xca, 0xf4,
+	0xdb, 0xe5, 0xa7, 0x99, 0x23, 0x1d, 0x5f, 0x61, 0x9f, 0xa1, 0xe3, 0xdd,
+	0x67, 0x59, 0x1b, 0x25, 0x0a, 0x34, 0x76, 0x48, 0xf2, 0xcc, 0x8e, 0xb0,
+	0xd0, 0xee, 0xac, 0x92, 0x28, 0x16, 0x54, 0x6a, 0x45, 0x7b, 0x39, 0x07,
+	0xbd, 0x83, 0xc1, 0xff};
+
+unsigned int crc8(unsigned int crc, unsigned char const *data, size_t len)
+{
+	if (data == NULL)
+		return 0;
+	crc &= 0xff;
+	unsigned char const *end = data + len;
+	while (data < end)
+		crc = crc8_table[crc ^ *data++];
+	return crc;
+}
+
 int check_crc(struct msg *msg)
 {
-	return 1;
+	if (msg->header.crc != crc8(msg->header.crc,
+				    msg->payload, msg->header.len))
+		return 1;
+
+	return 0;
 }
 
 static void receive_reply(int fd, struct msg *msg)
@@ -97,8 +137,8 @@ static void receive_reply(int fd, struct msg *msg)
 	}
 
 	ret = read(fd, msg->payload, msg->header.len);
-	if (ret > 0 && ret < msg->header.len) {
-		printf("server: read %d bytes expected %d\n", ret, msg->header.len);
+	if (ret > 0 && ret < (int) msg->header.len) {
+		printf("server: read %d bytes expected %lud\n", ret, msg->header.len);
 		exit(1);
 	} else if (ret < 0) {
 		perror("read error: ");
@@ -114,7 +154,7 @@ static void send_msg(int fd, int len)
 	memset(msg.payload, 0x55, len);
 	msg.header.type = PING_PONG;
 	msg.header.len = len;
-	msg.header.crc = 0;//get_crc(msg);
+	msg.header.crc = crc8(msg.header.crc, msg.payload, msg.header.len);
 
 	printf("Gonna write header %d\n", sizeof(struct msg_header));
 	ret = write(fd, &msg.header, sizeof(struct msg_header));
@@ -128,8 +168,8 @@ static void send_msg(int fd, int len)
 
 	printf("Gonna write payload %d\n", msg.header.len);
 	ret = write(fd, msg.payload, msg.header.len);
-	if (ret > 0 && ret < msg.header.len) {
-		printf("server: write %d bytes expected %d\n", ret, msg.header.len);
+	if (ret > 0 && ret < (int) msg.header.len) {
+		printf("server: write %d bytes expected %lud\n", ret, msg.header.len);
 		exit(1);
 	} else if (ret < 0) {
 		perror("server write error: ");
@@ -206,7 +246,7 @@ static void run_client(int fd, int len, int mors, int limit)
 	}
 }
 
-static void ping_pong(int fd, const char *payload, int len)
+static void ping_pong(int fd, const unsigned char *payload, int len)
 {
 	struct msg msg;
 	int ret;
@@ -214,7 +254,7 @@ static void ping_pong(int fd, const char *payload, int len)
 	memcpy(msg.payload, payload, len);
 	msg.header.type = PING_PONG;
 	msg.header.len = len;
-	msg.header.crc = 0;//get_crc(msg);
+	msg.header.crc = crc8(msg.header.crc, msg.payload, msg.header.len);
 
 	printf("Gonna write\n");
 	ret = write(fd, &msg.header, sizeof(struct msg_header));
@@ -228,8 +268,8 @@ static void ping_pong(int fd, const char *payload, int len)
 	}
 
 	ret = write(fd, msg.payload, msg.header.len);
-	if (ret > 0 && ret < msg.header.len) {
-		printf("server: write %d bytes expected %d\n", ret, msg.header.len);
+	if (ret > 0 && ret < (int) msg.header.len) {
+		printf("server: write %d bytes expected %lud\n", ret, msg.header.len);
 		exit(1);
 	} else if (ret < 0) {
 		perror("server write error: ");
@@ -261,8 +301,8 @@ static void run_server(int fd)
 		ret = read(fd, msg.payload, msg.header.len);
 		printf("Read payload\n");
 		err = errno;
-		if (ret > 0 && ret < msg.header.len) {
-			printf("server: read %d bytes expected %d\n", ret, msg.header.len);
+		if (ret > 0 && ret < (int) msg.header.len) {
+			printf("server: read %d bytes expected %lud\n", ret, msg.header.len);
 			exit(1);
 		} else if (ret < 0 && err != -60) {
 			perror("read error: ");
@@ -271,8 +311,8 @@ static void run_server(int fd)
 
 		printf("BEFORE CRC\n");
 		ret = check_crc(&msg);
-		if (!ret) {
-			printf("bad crc\n");
+		if (ret) {
+			printf("Bad CRC\n");
 			exit(1);
 		}
 
